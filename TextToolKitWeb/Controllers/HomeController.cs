982968c438c40c76a6e9b11c728968985dc2894e
Danielle Smith <smith.ddms@gmail.com>
2023-07-01 04:06:53 -0500

    Initial Commit

---
+++ TextToolKitWeb/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TextToolKitWeb.Models;
+
+namespace TextToolKitWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //Analyze
+        //Module Entry
+        public IActionResult AnalyzeEntry()
+        {
+            return View("AnalyzeEntry");
+        }
+        //Module Result
+        [HttpPost]
+        public ActionResult FormAnalyze(AnalyzeModel analyze)
+        {
+            ViewBag.UserEntry = analyze.UserEntry;
+            if (!string.IsNullOrEmpty(analyze.UserEntry))
+            {
+                analyze.CreateAnalysis(analyze.UserEntry);
+                ViewBag.LetterCount = analyze.LetterCount;
+                ViewBag.NumberCount = analyze.NumberCount;
+                ViewBag.OtherCount = analyze.OtherCount;
+                ViewBag.SpaceCount = analyze.SpaceCount;
+                ViewBag.TotalCount = analyze.TotalCount;
+                ViewBag.MostChar = analyze.MostChar;
+                ViewBag.LeastChar = analyze.LeastChar;
+                ViewBag.WordCount = analyze.WordCount;
+                ViewBag.MostWord = analyze.MostWord;
+                ViewBag.LeastWord = analyze.LeastWord;
+                ViewBag.WordList = analyze.WordList;
+                ViewBag.WordListCount = analyze.WordListCount;
+                ViewBag.AllCharList = analyze.AllCharList;
+                ViewBag.CharListCount = analyze.CharListCount;
+            }
+
+            return View("AnalyzeResult");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TextToolKitWeb/Models/AnalyzeModel.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace TextToolKitWeb.Models
+{
+    public class AnalyzeModel
+    {
+        public AnalyzeModel()
+        {
+        }
+
+        //From AnalyzeEntry View, to HomeController, then assigned here
+        public string UserEntry { get; set; }
+
+        //From HomeController, then assigned here, lastly goes to AnalyzeResult View
+        public int LetterCount { get; set; }
+        public int NumberCount { get; set; }
+        public int SpaceCount { get; set; }
+        public int OtherCount { get; set; }
+        public int TotalCount { get; set; }
+        public string MostChar { get; set; }
+        public string LeastChar { get; set; }
+
+        public int WordCount { get; set; }
+        public string MostWord { get; set; }
+        public string LeastWord { get; set; }
+
+        public List<string> AllCharList { get; set; }
+        public List<int> CharListCount { get; set; }
+
+        public List<string> WordList { get; set; }
+        public List<int> WordListCount { get; set; }
+
+        public void CreateAnalysis(string text)
+        {
+            //Character Data
+
+            //Char Counts
+            LetterCount = text.Where(x => char.IsLetter(x)).Count();
+            NumberCount = text.Where(x => char.IsNumber(x)).Count();
+            SpaceCount = text.Where(x => x == ' ').Count();
+            OtherCount = text.Where(x => !char.IsLetter(x) && !char.IsNumber(x) && x != ' ').Count();
+            TotalCount = text.Length;
+
+            //Char split, group, count
+            var splitChar = text.ToLower().Select(x => x.ToString());
+            var statsChar = splitChar.GroupBy(x => x).Select(x => x.Key).Select(x => new { Character = x, CharCount = splitChar.Where(y => y == x).Count() });
+
+            //Get the Most Used Character
+            int maxCount = statsChar.Max(x => x.CharCount);
+            MostChar = "";
+            if (statsChar.All(x => x.CharCount == maxCount))
+            {
+                MostChar = "All characters occur the same amount of times.";
+            }
+            else
+            {
+                var maxCharGroup = statsChar.Where(x => x.CharCount == maxCount).Select(x => x.Character).OrderBy(x => x);
+                MostChar = maxCharGroup.Count() > 1 ? string.Join(" ", maxCharGroup).Replace("  ", "Space ") : maxCharGroup.First().Replace(" ", "Space");
+                if (MostChar.Contains("Space"))
+                {
+                    if (MostChar == "Space")
+                    {
+                        int nextMaxCount = statsChar.Where(x => x.Character != " ").Max(x => x.CharCount);
+                        var nextMaxCharGroup = statsChar.Where(x => x.CharCount == nextMaxCount).Select(x => x.Character).OrderBy(x => x);
+                        MostChar += " followed by ";
+                        MostChar += nextMaxCharGroup.Count() > 1 ? string.Join(" ", nextMaxCharGroup) : nextMaxCharGroup.First();
+                    }
+                    else
+                    {
+                        MostChar = MostChar.Replace("Space", "").Trim();
+                        MostChar += " Space";
+                    }
+                }
+            }
+
+            //Get the Least Used Character
+            LeastChar = "";
+            if (MostChar == "All characters occur the same amount of times.")
+            {
+                LeastChar = "All characters occur the same amount of times.";
+            }
+            else
+            {
+                int minCount = statsChar.Min(x => x.CharCount);
+                var minCharGroup = statsChar.Where(x => x.CharCount == minCount).Select(x => x.Character).OrderBy(x => x);
+                LeastChar = minCharGroup.Count() > 1 ? string.Join(" ", minCharGroup).Replace("  ", "Space ") : minCharGroup.First().Replace(" ", "Space");
+                if (LeastChar.Contains("Space"))
+                {
+                    if (LeastChar == "Space")
+                    {
+                        int nextMinCount = statsChar.Where(x => x.Character != " ").Min(x => x.CharCount);
+                        var nextMinCharGroup = statsChar.Where(x => x.CharCount == nextMinCount).Select(x => x.Character).OrderBy(x => x);
+                        LeastChar += " followed by ";
+                        LeastChar += nextMinCharGroup.Count() > 1 ? string.Join(" ", nextMinCharGroup) : nextMinCharGroup.First();
+                    }
+                    else
+                    {
+                        LeastChar = MostChar.Replace("Space", "").Trim();
+                        LeastChar += " Space";
+                    }
+                }
+            }
+
+            //For Viewing Detailed Word and Word Count
+            AllCharList = "abcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+[]{}\\|;:'\",.<>/? ".Select(x => x.ToString()).ToList();
+            CharListCount = AllCharList.Select(x => statsChar.Where(y => y.Character == x.ToString()).Any() ? statsChar.Where(y => y.Character == x.ToString()).First().CharCount : 0).ToList();
+            AllCharList = AllCharList.Select(x => x == " " ? "Space" : x).ToList();
+
+            //Word Data
+
+            List<string> splitWords = new List<string>();
+            if (text.Contains(' '))
+            {
+                string replace = text.Replace('.', ' ').Replace('!', ' ').Replace('?', ' ').Replace(',', ' ').Replace(';', ' ').Replace(':', ' ');
+                if (replace.Any(x => x != ' '))
+                {
+                    splitWords = replace.ToLower().Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).ToList();
+                }
+            }
+
+            WordCount = splitWords.Count;
+
+            var statsWord = WordCount > 0 ? splitWords.GroupBy(x => x).Select(x => x.Key).Select(x => new { Word = x, WordCount = splitWords.Where(y => y == x).Count() }) : null;
+
+            MostWord = "";
+            LeastWord = "";
+
+            if (WordCount == 0)
+            {
+                MostWord = "Entry contains no words.";
+                LeastWord = "Entry contains no words.";
+            }
+            else
+            {
+                int maxWordCount = statsWord.Max(x => x.WordCount);
+                if (statsWord.All(x => x.WordCount == maxWordCount))
+                {
+                    MostWord = "All words occur the same amount of times.";
+                }
+                else
+                {
+                    var maxWordGroup = statsWord.Where(x => x.WordCount == maxWordCount).Select(x => x.Word).OrderBy(x => x);
+                    MostWord = maxWordGroup.Count() > 1 ? string.Join(", ", maxWordGroup) : maxWordGroup.First();
+                }
+
+                if (MostWord == "All words occur the same amount of times.")
+                {
+                    LeastWord = "All words occur the same amount of times.";
+                }
+                else
+                {
+                    int minWordCount = statsWord.Min(x => x.WordCount);
+                    var minWordGroup = statsWord.Where(x => x.WordCount == minWordCount).Select(x => x.Word).OrderBy(x => x);
+                    LeastWord = minWordGroup.Count() > 1 ? string.Join(", ", minWordGroup) : minWordGroup.First();
+                }
+            }
+
+            //For Viewing Detailed Word and Word Count
+            WordList = statsWord.OrderByDescending(x => x.WordCount).ThenBy(x => x.Word).Select(x => x.Word).ToList();
+            WordListCount = statsWord.OrderByDescending(x => x.WordCount).ThenBy(x => x.Word).Select(x => x.WordCount).ToList();
+        }
+    }
+}
